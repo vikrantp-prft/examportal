b5b2d3d52dd36729fc42fa4c9684b19f8118d7e1
Sumit Salpekar <sumit.salpekar@Perficient.com>
2019-01-16 13:26:24 +0530

    Add proper references in all the layers

--- API/Test/PerftEvaluation.Test/UnitTest1.cs
+++ API/Test/PerftEvaluation.Test/UnitTest1.cs
@@ -1,14 +1,11 @@
 using System;
 using Xunit;
 
-namespace PerftEvaluation.Test
-{
-    public class UnitTest1
-    {
+namespace PerftEvaluation.Test {
+    public class UnitTest1 {
         [Fact]
-        public void Test1()
-        {
-
+        public void Test1 () {
+            Assert.True (false);
         }
     }
 }
