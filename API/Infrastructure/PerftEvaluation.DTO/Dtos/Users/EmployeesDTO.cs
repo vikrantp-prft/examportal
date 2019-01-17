7fa3558e2dcd8484daf8402eec0f440ec292a62c
Vaishali Prachand <vaishali.prachand@perficient.com>
2019-01-17 17:26:18 +0530

    Created EmployeeDTO

---
+++ API/Infrastructure/PerftEvaluation.DTO/Dtos/Users/EmployeesDTO.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace PerftEvaluation.DTO.Dtos.Users
+{
+    public class EmployeesDTO
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime? DOB { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+        public string CurrentAddress1 { get; set; }
+        public string CurrentAddress2 { get; set; }
+        public string CurrentCity { get; set; }
+        public string CurrentState { get; set; }
+        public string CurrentPincode { get; set; }
+        public string Mobile { get; set; }
+        public string Team { get; set; }
+        public string Note { get; set; }
+        public string Intrest { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
