ecaba8e0c6982a7158cdf7e5dad4e274ab7dd035
Jai Bidkar <jai.bidkar@perficient.com>
2019-01-17 16:09:40 +0530

    Employees and Admin User UI, Routing and basic structure commit

---
+++ UI/ExamPortal/src/app/layout/user/employee/exployee-add-update/quickemployeeaddupdate.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'quick-employee-add-update',
+  templateUrl: './quickemployeeaddupdate.html'
+})
+export class QuickAddEmployeeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
