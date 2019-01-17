71e6b4e255fd8233d415fc248bb0d2c6e0d6b507
Jai Bidkar <jai.bidkar@perficient.com>
2019-01-17 16:07:12 +0530

    Employees and Admin User UI, Routing and basic structure commit

--- UI/ExamPortal/src/app/layout/user/user.module.ts
+++ UI/ExamPortal/src/app/layout/user/user.module.ts
@@ -5,9 +5,18 @@ import { UserRoutingModule } from './user-routing';
 import { PaginationModule } from 'ng2-bootstrap';
 import { EmployeeListComponent } from './employee/employee-list/employeelist.component';
 import { AddEmployeeComponent } from './employee/exployee-add-update/employeeaddupdate.component';
+import { QuickAddEmployeeComponent } from './employee/exployee-add-update/quickemployeeaddupdate.component';
+import {AdminUserListComponent} from './admin-user/admin-user-list/adminuserlist.component';
+import { AddAdminUserComponent } from './admin-user/admin-user-add-update/adminuseraddupdate.component';
 
 @NgModule({
     imports: [CommonModule, UserRoutingModule, PaginationModule.forRoot() ],
-    declarations: [UserComponent, EmployeeListComponent, AddEmployeeComponent]
+    declarations: [UserComponent,
+         EmployeeListComponent,
+         AddEmployeeComponent,
+         QuickAddEmployeeComponent,
+         AdminUserListComponent,
+         AddAdminUserComponent
+    ]
 })
 export class UserModule {}
