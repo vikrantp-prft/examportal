92e9720c26839f9f7bb89cf2c34a44a27d1eff4d
Sumit Salpekar <sumit.salpekar@Perficient.com>
2019-01-18 11:23:50 +0530

    Added Employee Repository

---
+++ API/Infrastructure/PerftEvaluation.DAL/Interface/Users/IEmployeeRepository.cs
@@ -0,0 +1,5 @@
+using System;
+
+namespace PerftEvaluation.DAL.Interface {
+    public interface IEmployeeRepository { }
+}
---
+++ API/Infrastructure/PerftEvaluation.DAL/Repositories/Users/EmployeeRepository.cs
@@ -0,0 +1,6 @@
+using System;
+using PerftEvaluation.DAL.Interface.Users;
+
+namespace PerftEvaluation.DAL.Repositories {
+    public class EmployeeRepository : IEmployeeRepository { }
+}
