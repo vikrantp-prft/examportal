f852fd76e24f10b97c8b3582b8016198fc3700c2
Vaishali Prachand <vaishali.prachand@perficient.com>
2019-01-18 11:48:25 +0530

    Commited IEmployeeRepository

--- API/Infrastructure/PerftEvaluation.DAL/Interface/Users/IEmployeeRepository.cs
+++ API/Infrastructure/PerftEvaluation.DAL/Interface/Users/IEmployeeRepository.cs
@@ -1,5 +1,52 @@
 using System;
+using System.Collections.Generic;
+using PerftEvaluation.Entities.POCOEntities;
 
 namespace PerftEvaluation.DAL.Interface {
-    public interface IEmployeeRepository { }
+    /// <summary>
+    /// User repository Interface
+    /// </summary>
+    public interface IEmployeeRepository { 
+        /// <summary>
+        /// List of Employee
+        /// </summary>
+        /// <returns>Employee List</returns>
+        IEnumerable<Users> GetEmployees ();
+
+        /// <summary>
+        /// Save Employee Detail
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        //bool SaveEmployee (Users users);
+
+        /// <summary>
+        /// Get Employee Detail by Id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        //Users GetEmployeeById (string Id);
+
+        /// <summary>
+        /// Update Employee details
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        //bool UpdateEmployee (Users users);
+
+        /// <summary>
+        /// Inactivate the users detail
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        //bool InactivateEmployee (string userId);
+
+        /// <summary>
+        /// Active the user detail
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        //bool ActiveEmployee (string userId);
+
+    }
 }
