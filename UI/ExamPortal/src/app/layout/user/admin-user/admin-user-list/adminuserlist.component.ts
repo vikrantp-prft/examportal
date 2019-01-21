6c50f3e5bd9eddfb219f6b4cc826e74106037d3a
Jai Bidkar <jai.bidkar@perficient.com>
2019-01-21 12:28:53 +0530

    Admin User list display and delete commit

--- UI/ExamPortal/src/app/layout/user/admin-user/admin-user-list/adminuserlist.component.ts
+++ UI/ExamPortal/src/app/layout/user/admin-user/admin-user-list/adminuserlist.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { Http } from '@angular/http';
 import { commonService } from 'src/app/common/services/common.service';
+import swal from 'sweetalert2';
+import { ToastrService } from 'ngx-toastr';
 
 interface paginationModel {
   currentPage: number;
@@ -28,6 +30,7 @@ export class AdminUserListComponent implements OnInit {
   public recordno = 0;
   public totalItems = 0;
   public employeeList = [];
+  toastr: any;
   constructor(public router: Router, private CommonService: commonService, public http: Http) {}
   // Function for  pagination
  setRecordPerPage(event: any): void {
@@ -65,15 +68,37 @@ export class AdminUserListComponent implements OnInit {
   fn_getEmployee(id) {
 
   }
-  fn_deleteAdminUser(id) {
-    const url = 'api/user/api/User/InactivateUser';
-    let d: any;
-    this.CommonService.fn_PostWithData(url, d).subscribe(
-      (data: any) => {
-          this.employeeList = data.data;
-      },
-      err => console.error(err),
-      () => {}
-    );
+  fn_deleteAdminUser(Id) {
+    if (Id != null) {
+      swal({
+        title: 'Are you sure?',
+        text: 'You want to delete the User!',
+        buttonsStyling: true,
+        confirmButtonClass: 'btn btn-success',
+        showCancelButton: true,
+        cancelButtonClass: 'btn btn-danger',
+        confirmButtonText: 'Yes, delete it!'
+      }).then(x => {
+        if (x.value = true) {
+          const url = 'api/User/InactivateUser';
+          const model = {
+            id: ''
+          };
+          model.id = Id;
+          this.fn_delfun(url, model);
+        }
+      });
+    }
+  }
+
+  // function for soft deleting the Admin User.
+  fn_delfun(url, data) {
+    this.CommonService.fn_PostWithData(data, url).subscribe((result: any) => {
+      const rs = result;
+      if ((result.message = 'Success')) {
+        this.toastr.success('User\'s details deleted successfully!');
+        this.fn_GetAdminUserList();
+      }
+    });
   }
 }
