96d0062dfe1c1b47184eb27c86fe1c25bc62f78b
Vishal Chaware <vishal.chaware@Perficient.com>
2019-02-12 22:10:16 +0530

    User Exam wireframe

--- UI/ExamPortal/src/app/user-exam/exam.component.ts
+++ UI/ExamPortal/src/app/user-exam/exam.component.ts
@@ -5,7 +5,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
     selector: 'app-exam',
     templateUrl: './exam.component.html',
-    animations: [routerTransition()]
+    styleUrls: ['./exam.component.css']
 })
 export class ExamComponent implements OnInit {
     constructor() {
