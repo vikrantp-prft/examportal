6e695a045f71db22c9511ab340441e823cad508b
Vishal Chaware <vishal.chaware@Perficient.com>
2019-02-13 14:10:56 +0530

    User exam - get question category

--- UI/ExamPortal/src/app/user-exam/exam.component.ts
+++ UI/ExamPortal/src/app/user-exam/exam.component.ts
@@ -12,37 +12,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ExamComponent implements OnInit {
     examID: any;
-    examDetail: any;
+    public examDetail: object;
     public examDetailUrl = "api/Exams/GetExamById";
-    public questionListUrl = 'api/Questions/listQuestionsByExamId';
-    questionModel: any;
-    constructor(public commonService: commonService, private route: ActivatedRoute) {
+    examName: any;
+    question = [];
+    questionCategory = [];
+    constructor(private CommonService: commonService, private route: ActivatedRoute) {
         this.route.params.subscribe(params => {
             this.examID = params['examId'];
         });
     }
-    ngOnInit() { }
+    ngOnInit() {
+        this.getExamDetails();
+        this.getQuestionList();
+    }
 
     getExamDetails() {
         const examDetailModel = {
             "id": this.examID
         }
-        this.commonService.fn_PostWithData(examDetailModel, this.examDetailUrl).subscribe((result: any) => {
+        this.CommonService.fn_PostWithData(examDetailModel, this.examDetailUrl).subscribe((result: any) => {
+            const rs = result;
+            if (rs.statusCode === 200) {
+                this.examDetail = rs.data;
+                this.examName = rs.data.title;
+                console.log(this.examName)
+            }
+        });
+    }
+    getQuestionList() {
+        const url = 'api/Questions/listQuestionsByExamId';
+        const questionModel = {
+            "id": this.examID
+        };
+        this.CommonService.fn_PostWithData(questionModel, url).subscribe((result: any) => {
             const rs = result;
             console.log(rs.data)
             if (rs.statusCode == 200) {
-                this.examDetail = rs.data;
+                this.question = rs.data;
+                console.log()
+                this.getAllQuestionCategory();
             }
         });
     }
-
-    fn_GetQuestionsList() {
-        this.questionModel.id = this.examID;
-        this.commonService.fn_PostWithData(this.questionModel, this.questionListUrl).subscribe((result: any) => {
-            const rs = result;
-            if (rs.statusCode == 200) {
-
-            }
-        });
+    getAllQuestionCategory(){
+        console.log(this.question.length)
+        const distinct = ( value, index, self) => {
+            return self.indexOf(value) === index;
+        }
+        for(let i=0; i < this.question.length;i++){
+            this.questionCategory[i] = this.question[i].category.name;
+        }
+        this.questionCategory = this.questionCategory.filter(distinct);
+        console.log(this.questionCategory)
     }
+    y
 }
