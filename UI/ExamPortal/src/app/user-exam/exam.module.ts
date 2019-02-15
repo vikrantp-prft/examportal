67b1175aa9d51650bcb9852518f2a399a79b7884
Vishal Chaware <vishal.chaware@Perficient.com>
2019-02-15 15:58:11 +0530

    User Exam - add google font

--- UI/ExamPortal/src/app/user-exam/exam.module.ts
+++ UI/ExamPortal/src/app/user-exam/exam.module.ts
@@ -4,12 +4,14 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { ExamRoutingModule } from './exam-routing.module';
 import { ExamComponent } from './exam.component';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
-        ExamRoutingModule
+        ExamRoutingModule,
+        NgxUiLoaderModule
     ],
     declarations: [ExamComponent]
 })
