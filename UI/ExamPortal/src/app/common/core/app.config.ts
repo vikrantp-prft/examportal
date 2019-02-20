08dcc5fa052ece8b7a0662dbba9fa5b30a59b0f9
Kapil Khadgi <kapil.khadgi@perficient.com>
2019-02-20 16:23:40 +0530

    corrected url's for stage environment.

--- UI/ExamPortal/src/app/common/core/app.config.ts
+++ UI/ExamPortal/src/app/common/core/app.config.ts
@@ -2,8 +2,8 @@ export const appConfig = {
   // local host port of WEB LAYER
 
   //  apiUrl: 'http://localhost:54690/', // url for local
-   apiUrl: 'http://localhost:7000/', // url for dev
-  // apiUrl: 'http://zil189:90/', // url for dev
+  // apiUrl: 'http://localhost:7000/', // url for dev
+   apiUrl: 'http://zil189:90/', // url for dev
   //apiUrl: 'http://zil395:8080/', // url for production
 
   pattern: {
