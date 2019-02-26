141a8cdce8e9292f378a2d1eceacd01fd8252db3
Sumit Salpekar <sumit.salpekar@Perficient.com>
2019-02-26 12:21:13 +0530

    Added is feedback option

--- API/Core/PerftEvaluation.Entities/POCOEntities/Exams/Exams.cs
+++ API/Core/PerftEvaluation.Entities/POCOEntities/Exams/Exams.cs
@@ -56,5 +56,7 @@ namespace PerftEvaluation.Entities.POCOEntities {
         [Required]
         public int TotalQuestions { get; set; }
 
+        [BsonElement ("isPaperPublic")]
+        public bool IsFeedback { get; set; }
     }
 }
