38efa48de9b3512299f49f18db3631bc853ed760
Prajakta <prajakta.themdeo@perficient.com>
2019-03-13 11:54:54 +0530

    Show percentage

--- UI/ExamPortal/src/app/user-exam/view-result/viewResult.component.ts
+++ UI/ExamPortal/src/app/user-exam/view-result/viewResult.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { commonService } from 'src/app/common/services/common.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
     selector: 'view-result',
@@ -12,8 +13,10 @@ export class ViewResultComponent implements OnInit {
     public userId: string;
     public resultData: any[] = [];
     public examId: string;
+    public userResultData: any;
+    public percentage: any;
 
-    constructor( private route: ActivatedRoute, private CommonService: commonService ) { 
+    constructor( private ngxService: NgxUiLoaderService, private route: ActivatedRoute, private CommonService: commonService ) { 
         this.route.params.subscribe(params => {
             this.examId = params["examId"];
             this.userId = params["userId"];
@@ -23,6 +26,7 @@ export class ViewResultComponent implements OnInit {
 
     ngOnInit(): void { 
         this.fn_getIndividualResult();
+        this.fn_GetResultList();
     }
 
     fn_getIndividualResult()
@@ -31,15 +35,40 @@ export class ViewResultComponent implements OnInit {
             "examId" : this.examId,
             "userId": this.userId            
         }
+        this.ngxService.start();
         const url = "api/Results/getIndividualUserResult";
         this.CommonService.fn_PostWithData(userModel, url).subscribe(
             (data: any) => {
             this.resultData = data;
+            this.ngxService.stop();
             //console.log(this.resultData);
             },
             err => console.error(err),
             () => {}
           );
     }
+
+    fn_GetResultList() {
+        const examModel = {
+          "id": this.examId
+        }
+        this.ngxService.start();
+        const url = "api/Results/listResultsByExamId"; 
+        this.CommonService.fn_PostWithData(examModel, url).subscribe(
+          (data: any) => {
+            this.userResultData = data.data;
+            console.log(this.userResultData);
+            console.log(this.userResultData[0].obtainedMarks);
+            this.percentage=(((this.userResultData[0].obtainedMarks)/(this.userResultData[0].totalMarks)*100).toFixed(2));
+            console.log(this.percentage);
+            this.ngxService.stop();
+            //this.userModel.totalRecords = data.totalRecords;
+          },
+          err => console.error(err),
+          () => {}
+        );
+      }
+
+      
     
 }
